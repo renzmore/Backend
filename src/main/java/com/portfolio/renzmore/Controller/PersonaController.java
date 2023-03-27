1f92e23e914ed321e30e3135d98ea78e9fe0771f
renzmore <rjmr33@gmail.com>
2023-03-27 18:02:40 -0300

    validando error de la seccion acerca de

--- src/main/java/com/portfolio/renzmore/Controller/PersonaController.java
+++ src/main/java/com/portfolio/renzmore/Controller/PersonaController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,28 +39,6 @@ public class PersonaController {
 
         Persona persona = personaService.getOne(id).get();
         return new ResponseEntity(persona, HttpStatus.OK);
-    }
-
-    @PostMapping("/crear")
-    public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona) {
-        if (personaService.existsByNombre(dtopersona.getNombre())) {
-            return new ResponseEntity(new Mensaje("Ese nombre ya existe"), HttpStatus.BAD_REQUEST);
-        }
-        if (StringUtils.isBlank(dtopersona.getNombre())) {
-            return new ResponseEntity(new Mensaje("El campo no puede estar vacio"), HttpStatus.BAD_REQUEST);
-        }
-
-        Persona persona = new Persona();
-
-        persona.setNombre(dtopersona.getNombre());
-        persona.setApellido(dtopersona.getApellido());
-        persona.setDescripcion(dtopersona.getDescripcion());
-
-        persona.setImg(dtopersona.getImg());
-
-        personaService.save(persona);
-
-        return new ResponseEntity(new Mensaje("Persona creada"), HttpStatus.OK);
     }
 
 
